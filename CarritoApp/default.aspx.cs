3c2e18b3dbfdb1cbe63faa8badf51ae2951ced40
JoaJufri <joaquinjufrida@gmail.com>
2023-05-31 00:51:29 -0300

    Se agrega primer filtro y codigo en postback

--- CarritoApp/default.aspx.cs
+++ CarritoApp/default.aspx.cs
@@ -7,20 +7,28 @@ using System.Web.Services.Description;
 namespace CarritoApp
 {
     public partial class WebForm1 : System.Web.UI.Page
-    {   
+    {
         public List<Articulo> listaArt { get; set; }
+        public List<Articulo> listaFiltrados { get; set; }
         public List<Imagen> listaImg { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
             ArticuloNegocio negocio = new ArticuloNegocio();
+            listaArt = new List<Articulo>();
+            listaFiltrados = new List<Articulo>();
             listaArt = negocio.listarconSP();
-            
+
+            if (!IsPostBack)
+            {
+                listaArt = negocio.listarconSP();
+            }
+
             ImagenNegocio negocioImg = new ImagenNegocio();
             listaImg = new List<Imagen>();
             foreach (Articulo ar in listaArt)
             {
                 Imagen aux = new Imagen();
-                aux=negocioImg.listar(ar.Id)[0];
+                aux = negocioImg.listar(ar.Id)[0];
                 listaImg.Add(aux);
             }
         }
@@ -31,14 +39,31 @@ namespace CarritoApp
         }
 
         protected void btFiltro_Click(object sender, EventArgs e)
-        {   
+        {
+            listaFiltrados = new List<Articulo>();
+            string criterio = ddl_Criterio.SelectedItem.Text;
+            if (criterio == "Marca")
+            {
+                foreach (var item in listaArt)
+                {
+                    if (item.Marca.ToString() == tbFiltro.Text.ToString())
+                    {
+                        listaFiltrados.Add(item);
+                    }
+                }
+                listaArt = listaFiltrados;
+
+            }
+            
             //Filtros
 
-            //string criterio = ddl_Criterio.SelectedItem.Text;
-            //if (criterio == "Marca") {
-                
-                
+
+            //Si no hay coincidencias, devuelve la lista original
+            //if (listaFiltrados.Count <= 0)
+            //{
+            //    listaArt = listaFiltrados; 
             //}
+
         }
     }
 }
